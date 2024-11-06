5902267c039f201277c2eaa3213dab474186905d
CʏʙᴇʀKɪʟʟᴇʀꜱ Tᴇᴀᴍ 👽 <161101124+CyberKillersTEAM@users.noreply.github.com>
2024-11-06 20:26:14 +0530

    Update config-db.js

--- config-db.js
+++ config-db.js
@@ -1,7 +1,7 @@
 
-const GITHUB_AUTH_TOKEN = ""
-const GITHUB_USERNAME = ""
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const GITHUB_USERNAME = "IsuruLK-007"
+const SESSION_ID = "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=njgRWTJQ#e0dmJNRwYRATDke_tFtQzSQfV2vL9_rKfHy1UJVab70"
 
 
 module.exports = {
